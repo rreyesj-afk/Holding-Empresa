033c61a4366ce842f6ece675daee9b6150ba9d41
rreyesj-afk <rreyesj@unicartagena.edu.co>
2026-05-15 23:14:41 -0500

    creación del proyecto

---
+++ src/main/java/com/edu/udec/holding/Main.java
@@ -0,0 +1,8 @@
+package com.edu.udec.holding;
+
+public class Main {
+
+    public static void Main (String[]args) {
+        System.out.println("Creación del proyecto. Empezamos desde cero.");
+    }
+}
